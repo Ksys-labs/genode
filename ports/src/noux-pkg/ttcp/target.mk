3643c6ca5593e939255f9312289bd4097d9af07b
Alexander Tarasikov <alexander.tarasikov@gmail.com>
2013-04-19 18:14:27 +0400

    Add ttcp patch for genode

--- ports/src/noux-pkg/ttcp/target.mk
+++ ports/src/noux-pkg/ttcp/target.mk
@@ -1,8 +1,11 @@
 TTCP_C   = ttcp.c
-TTCP_URL = http://sd.wareonearth.com/~phil/net/ttcp/ttcp.c
+#TTCP_URL = http://sd.wareonearth.com/~phil/net/ttcp/ttcp.c
+TTCP_ZIP = UnixTTCP.zip
+TTCP_URL = http://www.pcausa.com/Utilities/pcattcp/$(TTCP_ZIP)
 
 $(TTCP_C):
-	$(VERBOSE)wget -c $(TTCP_URL) && touch $@
+	$(VERBOSE)wget -c $(TTCP_URL) && unzip $(TTCP_ZIP) && mv UnixTTCP/$(TTCP_C) . && touch $@
+	$(VERBOSE)patch -p1 -i $(REP_DIR)/src/noux-pkg/ttcp/ttcp.patch 
 
 TARGET = ttcp
 SRC_C = $(TTCP_C)
