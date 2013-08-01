641cc8719a60d8398ff98593aed7f9935ed36f3a
Ivan Loskutov <ivan.loskutov@ksyslabs.org>
2013-08-01 21:41:15 +0300

    Add prototipe of init with signature check

---
+++ sss13/lib/mk/initsig_pd_args.mk
@@ -0,0 +1,5 @@
+SRC_CC = pd_args.cc
+
+LIBS += libc-static
+
+vpath pd_args.cc $(REP_DIR)/src/initsig
---
+++ sss13/src/initsig/target.mk
@@ -0,0 +1,3 @@
+TARGET = init
+SRC_CC = main.cc
+LIBS   = base initsig_pd_args micro-ecc sha2 libc-static
